96d4e591011a58458e9c9e25f0067ac71312a749
Tatiana Grigorjevschi <tatiana.grigorzh@gmail.com>
2023-07-02 12:02:32 +0200

    initial commit

---
+++ src/main/java/com/vacations/website/VacationsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.vacations.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationsWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacations/website/VacationsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacations.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
