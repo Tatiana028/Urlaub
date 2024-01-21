3dda44a88ee0296ae4a9daeaa6e0a2150cf2ee4e
Tatiana028 <tatiana.grigorzh@gmail.com>
2024-01-21 11:29:56 +0100

    connection to mainPage

--- src/main/java/com/vacations/website/VacationController.java
+++ src/main/java/com/vacations/website/VacationController.java
@@ -19,6 +19,14 @@ public class VacationController {
         return "add-airline";
     }
 
+    @GetMapping("/")
+    public String showStarterPage(Airline airline) {
+        return "userInterface/starterPage";
+    }
+
+
+
+
     @PostMapping("/add-airline")
     public String addAirline(Airline airline, Model model) {
         airlineRepository.save(airline);
