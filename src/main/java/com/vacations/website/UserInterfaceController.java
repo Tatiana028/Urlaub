c55e1257a9b3e1317eba5668b8ac8db27618f020
Tatiana028 <tatiana.grigorzh@gmail.com>
2024-04-22 20:29:13 +0200

    HTML changes

--- src/main/java/com/vacations/website/UserInterfaceController.java
+++ src/main/java/com/vacations/website/UserInterfaceController.java
@@ -1,26 +1,38 @@
 package com.vacations.website;
 
 import com.vacations.website.domain.Airline;
+import com.vacations.website.domain.Airport;
+import com.vacations.website.domain.AirportRepository;
 import com.vacations.website.domain.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserInterfaceController {
     @Autowired
     private FlightRepository flightRepository;
+    @Autowired //erzeugt eine Instanz
+    private AirportRepository airportRepository;
+
     @GetMapping("/")
-    public String showStarterPage(Airline airline) {
+    public String showStarterPage(Model model) {
+
+        List<Airport> airports = (List<Airport>) airportRepository.findAll();
+        model.addAttribute("airports", airports);
+        model.addAttribute("ap", new Airport());
         return "userInterface/starterPage";
     }
 
     @PostMapping("/show-flights")
-    public String showFlights(Model model){
-        String from = (String) model.getAttribute("from");
-
+    public String showFlights(@ModelAttribute Airport airport){
+        String from = airport.getAirportName();
+        System.out.println(from);
         return "userInterface/listOfFlights";
     }
 
